32d0b92dbfacdc0a680cfb37c9bbac2d655f6a36
tearjerky <yuhan.johncarlos@gmail.com>
2021-06-03 01:38:31 +0800

    upload files

---
+++ android/app/src/main/kotlin/com/example/amplify_socialnetwork/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.amplify_socialnetwork
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Social Network',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home:LogInPage(),
+    );
+  }
+}
+class LogInPage extends StatefulWidget {
+  const LogInPage({Key key}) : super(key: key);
+
+  @override
+  _LogInPageState createState() => _LogInPageState();
+}
+
+class _LogInPageState extends State<LogInPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
